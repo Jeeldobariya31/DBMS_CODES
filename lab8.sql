f7fd4fca0007bd70bf6edd1dcb6c4bc9cd799607
Jeeldobariya33 <jeeldobariya33@gmail.com>
2025-07-08 10:44:44 +0530

    added lab7 and lab8

---
+++ lab8.sql
@@ -0,0 +1,168 @@
+USE CSE_3A_232;
+CREATE TABLE SALES_DATA(
+    REGION VARCHAR(25),
+    PRODUCT VARCHAR(10),
+    SALES_AMT INT,
+    YEARS INT,
+)
+
+SELECT * FROM SALES_DATA
+INSERT INTO SALES_DATA(REGION , PRODUCT , SALES_AMT , YEARS)
+VALUES
+('North America' , 'Watch' ,1500 , 2023),
+('Europe' ,'Mobile',1200 , 2023 ),
+('Asia' , 'Watch' , 1800 , 2023),
+('North America' , 'TV' , 900 , 2024),
+('Europe' ,  'Watch', 2000, 2024),
+('Asia' , 'Mobile', 1000, 2024),
+('North America' ,  'Mobile', 1600, 2023),
+('Europe', 'TV', 1500, 2023),
+('Asia', 'TV', 1100, 2024),
+('North America', 'Watch', 1700, 2024)
+
+SELECT * FROM SALES_DATAS
+
+
+
+-- Part – A:
+-- 1. Display Total Sales Amount by Region.
+SELECT REGION, SUM(SALES_AMT) 
+FROM SALES_DATA
+GROUP BY REGION
+
+-- 2. Display Average Sales Amount by Product
+SELECT PRODUCT, AVG(SALES_AMT) 
+FROM SALES_DATA
+GROUP BY PRODUCT
+
+-- 3. Display Maximum Sales Amount by Year
+SELECT YEARS, MAX(SALES_AMT) 
+FROM SALES_DATA
+GROUP BY YEARS
+-- 4. Display Minimum Sales Amount by Region and Year
+SELECT REGION,YEARS ,  MIN(SALES_AMT) 
+FROM SALES_DATA
+GROUP BY REGION , YEARS
+
+-- 5. Count of Products Sold by Region
+SELECT REGION, COUNT(PRODUCT) AS TOTAL_PRODUCT
+FROM SALES_DATA
+GROUP BY REGION
+-- 6. Display Sales Amount by Year and Product
+SELECT YEARS , PRODUCT, SUM(SALES_AMT)
+FROM SALES_DATA
+GROUP BY YEARS , PRODUCT
+-- 7. Display Regions with Total Sales Greater Than 5000
+SELECT REGION, SUM(SALES_AMT) AS TOTAL_SALES 
+FROM SALES_DATA
+GROUP BY REGION
+HAVING SUM(SALES_AMT)>5000
+-- 8. Display Products with Average Sales Less Than 10000
+SELECT PRODUCT, AVG(SALES_AMT) 
+FROM SALES_DATA
+GROUP BY PRODUCT
+HAVING AVG(SALES_AMT)<10000
+    
+-- 9. Display Years with Maximum Sales Exceeding 500
+SELECT YEARS, MAX(SALES_AMT) 
+FROM SALES_DATA
+GROUP BY YEARS
+HAVING MAX(SALES_AMT)>500
+-- 10. Display Regions with at Least 3 Distinct Products Sold.
+SELECT REGION, COUNT(DISTINCT PRODUCT) AS Distinct_product
+FROM SALES_DATA
+GROUP BY REGION
+HAVING COUNT(DISTINCT PRODUCT) > 3
+-- 11. Display Years with Minimum Sales Less Than 1000
+SELECT Years, MIN(SALES_AMT)
+FROM SALES_DATA
+GROUP BY YEARS
+HAVING MIN(SALES_AMT)<1000
+
+-- 12. Display Total Sales Amount by Region for Year 2023, Sorted by Total Amount
+SELECT REGION, SUM(SALES_AMT) AS TOTAL_AMOUNT
+FROM SALES_DATA
+WHERE YEARS = 2023
+GROUP BY REGION
+ORDER BY TOTAL_AMOUNT ASC;
+
+-- 13. Find the Region Where 'Mobile' Had the Lowest Total Sales Across All Years.
+SELECT TOP(1) REGION, SUM(SALES_AMT) AS TOTAL_AMOUNT
+FROM SALES_DATA
+WHERE  PRODUCT= 'Mobile'
+GROUP BY REGION
+ORDER BY TOTAL_AMOUNT;
+
+
+-- 14. Find the Product with the Highest Sales Across All Regions in 2023.
+SELECT TOP 1 PRODUCT, SUM(SALES_AMT) AS TOTAL_AMOUNT
+FROM SALES_DATA
+WHERE YEARS = 2023
+GROUP BY PRODUCT
+ORDER BY TOTAL_AMOUNT DESC;
+-- 15. Find Regions Where 'TV' Sales in 2023 Were Greater Than 1000
+SELECT REGION, SUM(SALES_AMT) AS TOTAL_AMOUNT
+FROM SALES_DATA
+WHERE YEARS = 2023 AND PRODUCT='TV'
+GROUP BY REGION
+HAVING SUM(SALES_AMT) > 1000
+-------------------PART-B------------------
+--1. Display Count of Orders by Year and Region, Sorted by Year and Region 
+SELECT YEARS,REGION, COUNT(*) AS TOTAL
+FROM SALES_DATA
+GROUP BY YEARS,REGION
+ORDER BY  YEARS,REGION
+--2. Display Regions with Maximum Sales Amount Exceeding 1000 in Any Year, Sorted by Region 
+SELECT REGION, MAX(SALES_AMT) AS MAXIMUM
+FROM SALES_DATA
+GROUP BY REGION
+HAVING MAX(SALES_AMT) >1000
+ORDER BY REGION
+--3. Display Years with Total Sales Amount Less Than 10000, Sorted by Year Descending 
+SELECT YEARS, SUM(SALES_AMT) AS TOTAL
+FROM SALES_DATA
+GROUP BY YEARS
+HAVING SUM(SALES_AMT) <10000
+ORDER BY YEARS DESC;
+--4. Display Top 3 Regions by Total Sales Amount in Year 2024 
+SELECT TOP 3 REGION, SUM(SALES_AMT) AS TOTAL
+FROM SALES_DATA
+WHERE YEARS=2024
+GROUP BY REGION
+ORDER BY SUM(SALES_AMT) DESC;
+--5. Find the Year with the Lowest Total Sales Across All Regions.
+SELECT TOP 1 YEARS, SUM(SALES_AMT) AS TOTAL
+FROM SALES_DATA
+GROUP BY YEARS
+ORDER BY SUM(SALES_AMT);
+
+-------------------------------PARTC----------------------------
+--1. Display Products with Average Sales Amount Between 1000 and 2000, Ordered by Product Name 
+SELECT PRODUCT, AVG(SALES_AMT) AS AVG_SALES
+FROM SALES_DATA
+GROUP BY PRODUCT
+HAVING AVG(SALES_AMT) BETWEEN 1000 AND 2000
+ORDER BY PRODUCT;
+--2. Display Years with More Than 1 Orders from Each Region 
+SELECT REGION ,YEARS, COUNT(SALES_AMT) AS TOTAL
+FROM SALES_DATA
+GROUP BY REGION,YEARS
+HAVING  COUNT(SALES_AMT)>1
+--3. Display Regions with Average Sales Amount Above 1500 in Year 2023 sort by amount in descending. 
+SELECT REGION , AVG(SALES_AMT) AS AVERAGE
+FROM SALES_DATA
+WHERE YEARS=2023
+GROUP BY REGION
+HAVING  AVG(SALES_AMT)>1500
+ORDER BY AVG(SALES_AMT) DESC;
+--4. Find out region wise duplicate product.
+SELECT REGION ,PRODUCT
+FROM SALES_DATA
+GROUP BY REGION,PRODUCT
+HAVING  COUNT(*)>1
+
+--5. Find out year wise duplicate product. 
+SELECT YEARS ,PRODUCT
+FROM SALES_DATA
+GROUP BY YEARS,PRODUCT
+HAVING  COUNT(*)>1
